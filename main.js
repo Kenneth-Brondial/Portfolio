7f247394357ba72ed1ecbca035785c1c9c9726d9
Kenneth-Brondial <128760720+Kenneth-Brondial@users.noreply.github.com>
2023-03-24 08:40:08 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+// NavBar JavaScript
+function toggleMenu() {
+  var menuToggle = document.querySelector(".toggle");
+  var menu = document.querySelector(".menu");
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
+
+// Scroll Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "85px",
+  duration: 2000,
+  reset: true,
+});
+
+sr.reveal(".home-text", {});
+sr.reveal(".home-img", { delay: 200 });
+sr.reveal(".social", { delay: 200 });
+
+sr.reveal(".about-img", {});
+sr.reveal(".heading", {});
+sr.reveal(".about-text", { delay: 200 });
+
+sr.reveal(".services-content", {});
+
+sr.reveal(".work-text", {});
+sr.reveal(".work-img", { delay: 200 });
+
+sr.reveal(".app h1", {});
+sr.reveal(".app", { delay: 200 });
+
+sr.reveal("form input", {});
+sr.reveal("form textarea", {});
+sr.reveal(".app", { delay: 200 });
+
+// Dark Mode
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.scr = "images/sun.png";
+  } else {
+    icon.scr = "images/moon.png";
+  }
+};
